71482fda4150e65f484bafa1dfb6adc8802b1d9d
OUTERGA-MOUSTAFA <outergamoustafa@gmail.com>
2026-09-07 16:15:22 +0100

    SHELL-3 Ajouter le constructeur, getters et toString()

--- src/main/java/ma/youcode/lineperm/model/User.java
+++ src/main/java/ma/youcode/lineperm/model/User.java
@@ -1,7 +1,34 @@
 package ma.youcode.lineperm.service.User;
 class User{
-
+// variables
 	private login String;
 	private passwordHash String;
 
+// constructure
+    public __constract(String login, String passwordHash){
+        this.login = login;
+        this.passwordHash = passwordHash;
+    }
+
+//getters
+    public String getLogin(){
+        return login;
+    }
+    public String getPasswordHaash(){
+        return passwordHash;
+    }
+
+//setters
+    public void setLogin(String login){
+        this.login = login;
+    }
+    public void setPasswordHash(String passwordHash){
+        this.passwordHash = passwordHash;
+    }
+
+// override 
+    public String toString(String login, String passwordHash){
+        return this.login + ":" + this.passwordHash;
+    }
+    
 }

d215146914e52da159663648be5c7bb9cd63f65c
OUTERGA-MOUSTAFA <outergamoustafa@gmail.com>
2026-09-08 14:51:54 +0100

    SHELL-8 Implémenter la méthode login(login, password) dans UserService

--- src/main/java/ma/youcode/lineperm/service/UserService.java
+++ src/main/java/ma/youcode/lineperm/service/UserService.java
@@ -4,7 +4,15 @@ package ma.youcode.lineperm.service;
 class UserService {
 	public static void main(String [] args){
 
-	
+	  public User connecter(String login, String password) {
+        User user = users.get(login);
+        if (user == null) return null; // login inconnu
+
+        boolean check = BCrypt.checkpw(password, user.getPasswordHash());
+        if (!check) return null; // mauvais password (même message que login inconnu)
+
+        return user; // succès
+    }
 
 	}
 }

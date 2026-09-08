8d0f2628dfe4db6833692f5be3366d8aa19a25f0
OUTERGA-MOUSTAFA <outergamoustafa@gmail.com>
2026-09-08 16:13:44 +0100

    SHELL-20 Appeler loadUsers() dans le constructeur de UserService

--- src/main/java/ma/youcode/lineperm/service/UserService.java
+++ src/main/java/ma/youcode/lineperm/service/UserService.java
@@ -16,17 +16,8 @@ public class UserService {
 	private final Map<String, User> users = new HashMap<>();
     private final Path storagePath = Paths.get("users.txt");
 
-	public static void main(String [] args){
-
-	  public User connecter(String login, String password) {
-        User user = users.get(login);
-        if (user == null) return null; // login inconnu
-
-        boolean check = BCrypt.checkpw(password, user.getPasswordHash());
-        if (!check) return null; // mauvais password (même message que login inconnu)
-
-        return user; // succès
-	  }
+	public UserService() {
+        loadUsers(); // t9ra les comptes mn l'fichier kayn ula la
     }
 
 

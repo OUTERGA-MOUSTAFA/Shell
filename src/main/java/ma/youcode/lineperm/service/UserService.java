a9e596b48ebf7ad2a1da8f041cb8515b0f308db6
OUTERGA-MOUSTAFA <outergamoustafa@gmail.com>
2026-09-08 15:04:46 +0100

    SHELL-9 Vérifier si un utilisateur est déjà connecté (refuser la connexion si c'est le cas)

--- src/main/java/ma/youcode/lineperm/service/UserService.java
+++ src/main/java/ma/youcode/lineperm/service/UserService.java
@@ -1,7 +1,19 @@
 package ma.youcode.lineperm.service;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 class UserService {
+	private final Map<String, User> users = new HashMap<>();
+    private final Path storagePath = Paths.get("users.txt");
+
+
+	public boolean existe(String login) {
+        return users.containsKey(login);
+    }
+
 	public static void main(String [] args){
 
 	  public User connecter(String login, String password) {
@@ -12,7 +24,12 @@ class UserService {
         if (!check) return null; // mauvais password (même message que login inconnu)
 
         return user; // succès
+	  }
     }
 
-	}
+
+	public boolean isLoggedIn(User currentUser) {
+        return currentUser != null;
+    }
+
 }

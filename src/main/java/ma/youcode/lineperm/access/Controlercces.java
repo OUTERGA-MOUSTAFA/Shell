fbfd01246e9a7bb5f23446c129ff7089f4959b92
OUTERGA-MOUSTAFA <outergamoustafa@gmail.com>
2026-09-11 10:25:51 +0100

    SHELL-43 Implémenter estAutorise(User, FichierProtege, char)

--- src/main/java/ma/youcode/lineperm/access/Controlercces.java
+++ src/main/java/ma/youcode/lineperm/access/Controlercces.java
@@ -1,8 +1,46 @@
 package ma.youcode.lineperm.access;
+
+import ma.youcode.lineperm.model.FichierProtege;
+import ma.youcode.lineperm.model.User;
+
 /**
  * Controlercces
  */
 public class Controlercces {
 
-    
+    /**
+     * Retourne true si l'utilisateur a le droit demandé sur le fichier.
+     * @param droit 'r', 'w' ou 'd'
+     */
+    public static boolean estAutorise(User user, FichierProtege fichier, char droit){
+        if (user == null || fichier == null) {
+            return false;
+        }
+
+        boolean estProprio = user.getLogin().equals(fichier.getProprietaire());
+
+        if (estProprio) {
+            // Bloc de gauche uniquement
+            switch (droit) {
+                case 'r': return fichier.isrProp();
+                case 'w': return fichier.iswProp();
+                case 'd': return fichier.isdProp();
+                default:  return false;
+            }
+        } else {
+            // Bloc de droite uniquement
+            switch (droit) {
+                case 'r': return fichier.isrAutre();
+                case 'w': return fichier.iswAutre();
+                case 'd': return fichier.isdAutre();
+                default:  return false;
+            }
+        }
+
+    }
+
+    public static boolean estProprietaire(User user, FichierProtege fichier) {
+        if (user == null || fichier == null) return false;
+        return user.getLogin().equals(fichier.getProprietaire());
+    }
 }

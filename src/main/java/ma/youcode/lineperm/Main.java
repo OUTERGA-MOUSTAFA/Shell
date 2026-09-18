3796db330127ffafa494b16f92ae5c5854d801a9
OUTERGA-MOUSTAFA <outergamoustafa@gmail.com>
2026-09-18 15:05:41 +0100

    Learning streams on Part_3_testingFunctions also add to my code stats on switch and while, function on service/Logservice and that return Map List or nothing or long ... then print on same path or in ui/ConsoleApp, In ressources/acces.log some log to test stats, In model/AccessLog.java class of AccessLog and getters

--- src/main/java/ma/youcode/lineperm/Main.java
+++ src/main/java/ma/youcode/lineperm/Main.java
@@ -1,12 +1,15 @@
 package ma.youcode.lineperm;
 
+import ma.youcode.lineperm.service.LogService;
 import ma.youcode.lineperm.ui.ConsoleApp;
 
 public class Main{
+	
+	
 	public static void main(String [] args){
 		
 		ConsoleApp app = new  ConsoleApp();
-
+		
 		 app.demarrer();
 	}
 }

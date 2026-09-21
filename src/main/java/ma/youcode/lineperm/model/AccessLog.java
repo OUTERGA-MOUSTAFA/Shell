1b50cf002a2437c3ab15bc72e54654720151e423
OUTERGA-MOUSTAFA <outergamoustafa@gmail.com>
2026-09-21 18:09:44 +0100

    feat(model): aligner  Log avec la BDD

--- src/main/java/ma/youcode/lineperm/model/AccessLog.java
+++ src/main/java/ma/youcode/lineperm/model/AccessLog.java
@@ -5,35 +5,38 @@ package ma.youcode.lineperm.model;
  */
 public class AccessLog {
 
-     
-    private final String date;
-    private final String heure;
-    private final String utilisateur;
-    private final String action;   // LECTURE, ECRITURE, SUPPRESSION
-    private final String fichier;
+    private int id;
+    private final int utilisateurId;
+    private final int fichierId;
+    private final String dateAction;
+    // private final String utilisateur;
+    private final String action; // LECTURE, ECRITURE, SUPPRESSION
+    // private final String fichier;
     private final String resultat; // true => accept || false => refusé
 
-    public AccessLog(String date, String heure, String utilisateur, String action, String fichie,
-        String resultat
-    ){
-        this.date = date;
-        this.heure = heure;
-        this.utilisateur = utilisateur;
+    public AccessLog(int utilisateurId, int fichierId, String action, String resultat){
+        this(0, utilisateurId, fichierId, action, resultat, null);
+    }
+    public AccessLog(int id, int utilisateurId, int fichierId,
+                     String action, String resultat, String dateAction) {
+        this.id = id;
+        this.utilisateurId = utilisateurId;
+        this.fichierId = fichierId;
         this.action = action;
-        this.fichier = fichie;
+        this.dateAction = dateAction;
         this.resultat = resultat;
     }
 
-     public String getDate() { return date; }
-    public String getHeure() { return heure; }
-    public String getUtilisateur() { return utilisateur; }
+    public int getId() { return id; }
+    public int getUtilisateurId() { return utilisateurId; }
+    public int getFichierId() { return fichierId; }
     public String getAction() { return action; }
-    public String getFichier() { return fichier; }
     public String getResultat() { return resultat; }
+    public String getDateAction() { return dateAction; }
 
     @Override
     public String toString() {
-        return date + ";" + heure + ";" + utilisateur + ";" +
-               action + ";" + fichier + ";" + resultat;
+        return "Fichier Id :" + this.getFichierId() + "; Utilisateur Id: " + getUtilisateurId() + "; action: " + this.getDateAction() + ";" +
+                 "Date action: " + this.getDateAction() + ";" + "resultat: " + this.getResultat();
     }
 }

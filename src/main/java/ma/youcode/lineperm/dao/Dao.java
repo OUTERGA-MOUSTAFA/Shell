aa6600322756b444d72de4848f0bec7d95f7c5d2
OUTERGA-MOUSTAFA <outergamoustafa@gmail.com>
2026-09-21 16:00:53 +0100

    feat(dao): ajouter interface Dao<T> T = entity exp: User, Logs, File

---
+++ src/main/java/ma/youcode/lineperm/dao/Dao.java
@@ -0,0 +1,14 @@
+package ma.youcode.lineperm.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao {
+
+//  T = type de l'entité (User, FichierProtege, AccessLog).
+    T save(T entity);
+    Optional<T> findById(int id);
+    List<T> findAll();
+    boolean delete(int id);
+    
+}

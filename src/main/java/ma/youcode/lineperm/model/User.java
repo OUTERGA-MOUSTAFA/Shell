9af2b844663311c699938f12f3c408621f5c0ebf
OUTERGA-MOUSTAFA <outergamoustafa@gmail.com>
2026-09-21 16:15:34 +0100

    udpate: update Model User add id

--- src/main/java/ma/youcode/lineperm/model/User.java
+++ src/main/java/ma/youcode/lineperm/model/User.java
@@ -1,22 +1,25 @@
 package ma.youcode.lineperm.model;
 public  class User{
 // variables
-	private String login;
-	private String passwordHash ;
+private int id;
+	private final String login;// kayn ghir wahed login l wahed user t instansa
+	private  String passwordHash ;
 
 // constructure
     public User(String login, String passwordHash){
+        this(0, login, passwordHash);
+    }
+    public User(int id, String login, String passwordHash) {
+        this.id = id;
         this.login = login;
         this.passwordHash = passwordHash;
     }
 
 //getters
-    public String getLogin(){
-        return login;
-    }
-    public String getPasswordHash(){
-        return passwordHash;
-    }
+
+    public int getId() { return id; }
+    public String getLogin(){return login;}
+    public String getPasswordHash(){return passwordHash;}
 
 //setters
     public void setLogin(String login){

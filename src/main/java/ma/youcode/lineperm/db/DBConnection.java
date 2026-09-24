7cad70d0d22583e696ddb781ba0e9029e3644633
OUTERGA-MOUSTAFA <outergamoustafa@gmail.com>
2026-09-24 20:47:35 +0000

    feat:change connection from Drive Manager to DataSource Simple pool

--- src/main/java/ma/youcode/lineperm/db/DBConnection.java
+++ src/main/java/ma/youcode/lineperm/db/DBConnection.java
@@ -4,27 +4,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import org.sqlite.SQLiteDataSource;
 
 public class DBConnection {
     private static DBConnection instance;
-    private Connection connection;
+    // private Connection connection;
+    private final SQLiteDataSource dataSource;
+
     private static final String URL = "jdbc:sqlite:auditdb.db";// SQLite crée automatiquement le fichier auditdb.db s'il n'existe pas
 
-    private DBConnection(){
+    private DBConnection() {
 
-        try {
-            Class.forName("org.sqlite.JDBC");
-            this.connection = DriverManager.getConnection(URL);
-            try (Statement st = connection.createStatement()) {
-                st.execute("PRAGMA foreign_keys = ON");
-            }
-            initSchema();
-        } catch (ClassNotFoundException | SQLException e) {
-            throw new RuntimeException("Impossible de se connecter à SQLite : " + e.getMessage(), e);
-        }
+        // Créer le DataSource une seule fois
+        dataSource = new SQLiteDataSource();
+        dataSource.setUrl(URL);
+
+        initSchema();
+        // try {
+        // Class.forName("org.sqlite.JDBC");
+        // this.connection = DriverManager.getConnection(URL);
+        // try (Statement st = connection.createStatement()) {
+        // st.execute("PRAGMA foreign_keys = ON");
+        // }
+        // initSchema();
+        // } catch (ClassNotFoundException | SQLException e) {
+        // throw new RuntimeException("Impossible de se connecter à SQLite : " +
+        // e.getMessage(), e);
+        // }
 
     }
-
 
     public static synchronized DBConnection getInstance() {
         if (instance == null) {
@@ -36,12 +44,18 @@ public class DBConnection {
     // ouvrire la connexion à la bd
     public Connection getConnection() {
         try {
-            if (connection == null || connection.isClosed()) {
-                connection = DriverManager.getConnection(URL);
-                try (Statement st = connection.createStatement()) {
-                    st.execute("PRAGMA foreign_keys = ON");
-                }
-            }
+            // if (connection == null || connection.isClosed()) {
+            // connection = DriverManager.getConnection(URL);
+            // try (Statement st = connection.createStatement()) {
+            // st.execute("PRAGMA foreign_keys = ON");
+            // }
+            // }
+
+            /**
+             * Retourne une connexion.
+             * SQLiteDataSource gère la création/reconnexion otomatikman.
+             */
+            return dataSource.getConnection();
         } catch (SQLException e) {
             throw new RuntimeException("Connexion perdue : " + e.getMessage(), e);
         }
@@ -51,39 +65,39 @@ public class DBConnection {
     /** Crée les tables si elles n'existent pas (premier lancement). */
     private void initSchema() {
         String users = """
-            CREATE TABLE IF NOT EXISTS users (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                login TEXT UNIQUE NOT NULL,
-                password_hash TEXT NOT NULL
-            )
-        """;
+                    CREATE TABLE IF NOT EXISTS users (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        login TEXT UNIQUE NOT NULL,
+                        password_hash TEXT NOT NULL
+                    )
+                """;
 
         String fichiers = """
-            CREATE TABLE IF NOT EXISTS fichiers (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                nom TEXT NOT NULL,
-                proprietaire_id INTEGER NOT NULL,
-                droits_proprio TEXT NOT NULL DEFAULT 'rwd',
-                droits_autres  TEXT NOT NULL DEFAULT '---',
-                contenu TEXT DEFAULT '',
-                FOREIGN KEY (proprietaire_id) REFERENCES users(id)
-            )
-        """;
+                    CREATE TABLE IF NOT EXISTS fichiers (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        nom TEXT NOT NULL,
+                        proprietaire_id INTEGER NOT NULL,
+                        droits_proprio TEXT NOT NULL DEFAULT 'rwd',
+                        droits_autres  TEXT NOT NULL DEFAULT '---',
+                        contenu TEXT DEFAULT '',
+                        FOREIGN KEY (proprietaire_id) REFERENCES users(id)
+                    )
+                """;
 
         String logs = """
-            CREATE TABLE IF NOT EXISTS logs (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                utilisateur_id INTEGER NOT NULL,
-                fichier_id INTEGER NOT NULL,
-                action TEXT NOT NULL,
-                resultat TEXT NOT NULL,
-                date_action TEXT NOT NULL DEFAULT (datetime('now')),
-                FOREIGN KEY (utilisateur_id) REFERENCES users(id),
-                FOREIGN KEY (fichier_id)     REFERENCES fichiers(id)
-            )
-        """;
+                    CREATE TABLE IF NOT EXISTS logs (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        utilisateur_id INTEGER NOT NULL,
+                        fichier_id INTEGER NOT NULL,
+                        action TEXT NOT NULL,
+                        resultat TEXT NOT NULL,
+                        date_action TEXT NOT NULL DEFAULT (datetime('now')),
+                        FOREIGN KEY (utilisateur_id) REFERENCES users(id),
+                        FOREIGN KEY (fichier_id)     REFERENCES fichiers(id)
+                    )
+                """;
 
-        try (Statement st = connection.createStatement()) {
+        try (Connection conn = dataSource.getConnection(); Statement st = conn.createStatement()) {
             st.execute(users);
             st.execute(fichiers);
             st.execute(logs);
